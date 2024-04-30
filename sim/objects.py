ff307773fc2228ef21904bbdea0e162a2f64f37a
Tsunami014 <max.worrall@education.nsw.gov.au>
2024-05-01 08:43:53 +1000

    Added a centre dot to the objects!

--- sim/objects.py
+++ sim/objects.py
@@ -4,9 +4,14 @@ class Obj:
     def __init__(self, colour, points):
         self.colour = colour
         self.points = points
+        self.centre = self.find_centre()
+    
+    def find_centre(self):
+        return (sum([i[0] for i in self.points]) / len(self.points), sum([i[1] for i in self.points]) / len(self.points))
     
     def moveby(self, x, y):
         self.points = [(i[0]+x, i[1]+y) for i in self.points]
+        self.centre = self.find_centre()
     
     def draw(self, win):
         if len(self.points) < 2:
@@ -16,3 +21,4 @@ class Obj:
             pygame.draw.line(win, self.colour, prev, i, 10)
             prev = i
         pygame.draw.line(win, self.colour, self.points[-1], self.points[0], 10)
+        pygame.draw.circle(win, self.colour, self.centre, 10)
