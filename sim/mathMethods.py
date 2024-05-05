94312a6f8804ca761fad80ec4486e3c8957b465c
Tsunami014 <max.worrall@education.nsw.gov.au>
2024-05-06 08:01:45 +1000

    Removed the fixangle on the toPolar

--- sim/mathMethods.py
+++ sim/mathMethods.py
@@ -48,5 +48,5 @@ def fixangle(angle):
 def toPolar(origin, point): # Thanks to https://stackoverflow.com/questions/20924085/python-conversion-between-coordinates
     x, y = point[0] - origin[0], point[1] - origin[1]
     rho = math.sqrt(x**2 + y**2)
-    phi = fixangle(math.degrees(math.atan2(y, x)))
+    phi = math.degrees(math.atan2(y, x)) % 360
     return (rho, phi) # rho: distance, phi: angle
